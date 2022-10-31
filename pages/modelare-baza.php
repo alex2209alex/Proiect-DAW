0782d711f4cdb0faadc2fd071919bba911575cf1
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-10-31 23:13:11 +0200

    Schimbare titlu pagina modelare-baza.php

--- pages/modelare-baza.php
+++ pages/modelare-baza.php
@@ -6,8 +6,7 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <h5>Descriere succinta a bazei de date</h5>
-            <p>Diagrama entitate relatie a bazei de date</p>
+            <h5>Diagrama entitate relatie a bazei de date</h5>
             <img src="/images/diagrama-er.jpg" class="img-fluid" alt="diagrama er">
         </div>
     </div>

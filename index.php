cd946de1d3c70ce73a334e2a4d324d7e3cf993d4
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-10-31 23:19:16 +0200

    Adaugari titlu pagina principala

--- index.php
+++ index.php
@@ -3,5 +3,12 @@
     <?php include 'templates/head.php';?>
     <body>
         <?php include 'templates/menu.php';?>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h5>Pagina principala</h5>
+                </div>
+            </div>
+        </div>
     </body>
 </html>

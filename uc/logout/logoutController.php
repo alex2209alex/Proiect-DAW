b6f0d3fcb02532f8ea328a0513573db11a550e04
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-12-21 18:29:18 +0200

    Loggout da redirect catre pagina principala

--- uc/logout/logoutController.php
+++ uc/logout/logoutController.php
@@ -3,12 +3,12 @@
 session_start();
 
 if (!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]) {
-    header("location: loginPage.php");
+    header("location: /index.php");
     exit;
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     session_destroy();
-    header("location: loginPage.php");
+    header("location: /index.php");
     exit;
 }
